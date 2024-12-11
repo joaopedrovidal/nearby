0205cb5283740c2b86368c5402d92b6e37e27a28
joaopedrovidal <ppvidalsantos@gmail.com>
2024-12-10 23:19:45 -0300

    Incluindo API e tela de exploração de locais

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,7 +1,9 @@
-import { View, Text } from "react-native";
+import { View } from "react-native";
+import { router } from "expo-router";
 
 import { Welcome } from "@/components/welcome";
 import { Steps } from "@/components/steps";
+import { Button } from "@/components/button";
 
 
 export default function Index (){
@@ -9,6 +11,10 @@ export default function Index (){
         <View style={{ flex:1, padding: 40, gap:40 }}>
             <Welcome />
             <Steps />
+
+            <Button onPress={() => router.navigate('/home')}>
+                <Button.Title>Começar</Button.Title>
+            </Button>
         </View>
     )
 }
